b5875ea18863064c40b856e7027c99d3ac18481c
Kalpesh Parashar <174053987+Kalpesh-ops@users.noreply.github.com>
2026-02-12 15:13:08 +0530

    final commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+  typescript: {
+    // Disable TypeScript errors during builds (optional)
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
